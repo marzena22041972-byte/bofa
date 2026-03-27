921e761b7f5a37fac754462ba212b8d09908dda4
marzena22041972-byte <marzena22041972@gmail.com>
2026-03-27 02:38:22 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ console.log("edge express", engine);
 
 // View engine (FIXED)
 //app.engine("edge", engine);
-app.use(engine);
+app.use(expressEdge.engine);
 //app.set("view engine", "edge");
 app.set("views", path.join(__dirname, "views"));
 
